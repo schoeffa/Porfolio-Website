74c2b63d4f2baa1639b7ca717c7d8b6dfac0e728
aaron <schoeffa@gmail.com>
2018-12-29 15:11:14 -0500

    added and styled socials media marks/links

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 "use strict";
 
-var paths = document.querySelectorAll(".logo");
+let paths = document.querySelectorAll(".logo");
 
 for (let path of paths) {
-    var length = path.getTotalLength();
+    let length = path.getTotalLength();
     path.style.strokeDasharray = length + ' ' + length;
     path.style.strokeDashoffset = length;
     path.getBoundingClientRect();
     path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 3s ease-in-out';
     path.style.strokeDashoffset = '0';
 }
+
